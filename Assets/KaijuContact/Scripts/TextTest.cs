c8cc7b9252104fe1b937c91cef600562524dbd57
theishiopian <theishiopian@gmail.com>
2020-05-25 11:45:31 -0700

    Scrolling text test

---
+++ Assets/KaijuContact/Scripts/TextTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class TextTest : MonoBehaviour
+{
+    public bool running = true;
+    TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        StartCoroutine("coroutine");
+    }
+
+    int lines = 0;
+    string buffer = "";
+
+    IEnumerator coroutine()
+    {
+        while(running)
+        {
+            
+            for(int i = 0; i != 39; i++)
+            {
+                buffer += Random.Range(0,10);
+                text.SetText(buffer);
+                yield return new WaitForSeconds(0.001f);
+            }
+            buffer += "\n";
+            if (lines > 26)
+            {
+                buffer = buffer.Substring(40);
+            }
+            Debug.Log("line done");
+            lines++;
+            text.SetText(buffer);
+            //yield return new WaitForSeconds(0.001f);
+        }
+
+        yield return null;
+    }
+
+}
