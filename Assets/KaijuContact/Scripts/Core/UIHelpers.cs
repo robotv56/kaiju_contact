5cc943012e1a3761cc36ee5d091bab08e2621eb4
theishiopian <theishiopian@gmail.com>
2020-05-31 15:39:07 -0700

    Targetting square test
    Need full mesh from ray

---
+++ Assets/KaijuContact/Scripts/Core/UIHelpers.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UIHelpers
+{
+    //library code
+    static Vector3[] screenSpaceCorners;
+    public static Rect RendererBoundsInScreenSpace(Renderer renderer, Camera camera)
+    {
+        // This is the space occupied by the object's visuals
+        // in WORLD space.
+        Bounds bigBounds = renderer.bounds;
+
+        if (screenSpaceCorners == null)
+            screenSpaceCorners = new Vector3[8];
+
+        // For each of the 8 corners of our renderer's world space bounding box,
+        // convert those corners into screen space.
+        screenSpaceCorners[0] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z));
+        screenSpaceCorners[1] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z));
+        screenSpaceCorners[2] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z));
+        screenSpaceCorners[3] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x + bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z));
+        screenSpaceCorners[4] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z));
+        screenSpaceCorners[5] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y + bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z));
+        screenSpaceCorners[6] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z + bigBounds.extents.z));
+        screenSpaceCorners[7] = camera.WorldToScreenPoint(new Vector3(bigBounds.center.x - bigBounds.extents.x, bigBounds.center.y - bigBounds.extents.y, bigBounds.center.z - bigBounds.extents.z));
+
+        // Now find the min/max X & Y of these screen space corners.
+        float min_x = screenSpaceCorners[0].x;
+        float min_y = screenSpaceCorners[0].y;
+        float max_x = screenSpaceCorners[0].x;
+        float max_y = screenSpaceCorners[0].y;
+
+        for (int i = 1; i < 8; i++)
+        {
+            if (screenSpaceCorners[i].x < min_x)
+            {
+                min_x = screenSpaceCorners[i].x;
+            }
+            if (screenSpaceCorners[i].y < min_y)
+            {
+                min_y = screenSpaceCorners[i].y;
+            }
+            if (screenSpaceCorners[i].x > max_x)
+            {
+                max_x = screenSpaceCorners[i].x;
+            }
+            if (screenSpaceCorners[i].y > max_y)
+            {
+                max_y = screenSpaceCorners[i].y;
+            }
+        }
+
+        return Rect.MinMaxRect(min_x, min_y, max_x, max_y);
+    }
+}
---
+++ Assets/TargetOutline.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetOutline : MonoBehaviour
+{
+    public new Camera camera;
+    public GameObject target;
+
+    private RectTransform targetRect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRect = GetComponent<RectTransform>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Rect visualRect = UIHelpers.RendererBoundsInScreenSpace(target.GetComponent<MeshRenderer>(), camera);
+
+        //targetRect.position = new Vector2(visualRect.xMin, visualRect.yMin);
+
+        targetRect.sizeDelta = new Vector2(visualRect.width, visualRect.height);
+    }
+}
