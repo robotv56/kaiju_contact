c944dd1aeac9d3250951f12b578b228d59dbd96f
theishiopian <theishiopian@gmail.com>
2020-06-02 15:53:42 -0700

    Finished Lobby Screen

---
+++ Assets/KaijuContact/Scripts/Menu/TextReveal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextReveal : MonoBehaviour
+{
+    public float delay = 0.1f;
+
+    private TextMeshProUGUI text;
+
+    private int count;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        count = text.text.Length;
+        text.maxVisibleCharacters = 0;
+        StartCoroutine("Coroutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Coroutine()
+    {
+        for(int i = 0; i != count; i++)
+        {
+            text.maxVisibleCharacters += 1;
+            if(text.text[i] != '\n' && text.text[i] != ' ')yield return new WaitForSeconds(delay);
+        }
+
+        yield return null;
+    }
+}
